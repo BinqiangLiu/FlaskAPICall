ab023fb4324937f8d1ab10f46ea54fdba79504bd
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-10-25 14:10:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ with open(css_file) as f:
     st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
 def call_chatbot_api(query):
-  #url = 'https://binqiangliu-flaskapi-in-docker.hf.space/api/chat' #Flask API调用成功
+  url = 'https://binqiangliu-flaskapi-in-docker.hf.space/api/chat' #Flask API调用成功
   #url = 'https://binqiangliu-flaskapi-in-docker.hf.space/' #Flask API调用成功
 
   url = 'https://ishare-flaskapi-in-docker.hf.space/api/chat' #Flask API调用成功
@@ -20,15 +20,17 @@ def call_chatbot_api(query):
     #故障的原因，需要回到API程序（即url指向的App）查看：raise ValueError(f"Error raised by inference API: {response['error']}")
     #ValueError: Error raised by inference API: Model is overloaded - 调用频次过高，导致模型过载（估计主要就是因为App在Free tier的CPU硬件资源运行所致吧？）
  
-  #url = 'https://binqiangliu-flaskapi-streamlitsdk.hf.space/api/chat' #Flask API调用???
+  #url = 'https://binqiangliu-flaskapi-streamlitsdk.hf.space/api/chat' #Flask API调用失败！
   #https://huggingface.co/spaces/binqiangliu/FlaskAPI-StreamlitSDK
+  #初步判定原因是由于其SDK是Streamlit所致（这个和在Github部署的时候，在app.py中还有Streamlit的代码，却能够成功部署到Render并与成功构建API App不同！
+  #这个原因，就和将API App部署到Streamlit的Cloud平台一样无法工作一样！所以，综合结论就是：Streamlit和FlaskAPI/FastAPI不兼容！
     
   #url = 'https://binqiangliu-flask-inference-api.hf.space/api/chat'  #Flask API调用成功 - 直接在app.py中采用Flask App的app.run(host='0.0.0.0',  port=7860)，没有采用uvicorn或gunicorn
   #url = 'https://binqiangliu-flask-inference-api.hf.space/' #Flask API调用成功 - 直接在app.py中采用Flask App的app.run(host='0.0.0.0',  port=7860)，没有采用uvicorn或gunicorn
     
   #url = 'https://binqiangliu-flaskdeployhf.hf.space/api/chat' #Flask API调用-N/A    
     
-  url='https://hf-aichat-api.onrender.com/api/chat'   #Flask API调用???(https://github.com/BinqiangLiu/OpenAIChat/edit/main/api.py)
+  #url='https://hf-aichat-api.onrender.com/api/chat'   #Flask API调用成功(https://github.com/BinqiangLiu/OpenAIChat/edit/main/api.py)
     
   json_data_for_api = {'user_question': query}
   response = requests.post(url, json=json_data_for_api) 
